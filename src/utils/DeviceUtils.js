a91ef03481bfc7c9214b4e93a34651c2749b596b
Muhammad Hafif Al Busyro <muhammadhafifalbusyro@gmail.com>
2022-12-08 01:09:08 +0700

    bug fix ios

--- src/utils/DeviceUtils.js
+++ src/utils/DeviceUtils.js
@@ -1,5 +1,5 @@
 // DeviceInfo adalah modul native Android (Java) atau Ios (Swift) yang digunakan untuk mengambil fungsi-fungsi native dalam mendapatkan data device user
-import DeviceInfo from 'hafif-package-device-info';
+import DeviceInfo from 'react-native-device-info';
 import {Platform} from 'react-native';
 
 // Fungsi getDeviceModel() adalah fungsi yang digunakan untuk mendapatkan data model dan OS device yang digunakan oleh user
